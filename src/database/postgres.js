27bba02be6e5ad4f2033fb524547e2bbd8b4537a
gr153tt@gmail.com <gr153tt@gmail.com>
2021-09-04 18:05:24 -0300

    setting up enviroviment varaibles, the most organized way possible

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -1,11 +1,19 @@
 import { Pool } from 'pg';
 
+import {
+  DB_USER,
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+} from '../environment';
+
 const pool = new Pool({
-  user: 'gabriel',
-  host: 'localhost',
-  database: 'instagram',
-  password: 'gabriel153tt@',
-  port: 5432
-})
+  user: DB_USER || 'gabriel',
+  host: DB_HOST || 'localhost',
+  database: DB_NAME || 'instagram',
+  password: DB_PASSWORD || 'gabriel',
+  port: DB_PORT || 5432,
+});
 
 export default pool;
---
+++ src/environment/index.js
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+
+// AWS credentials enviroviment variables
+const {
+  AWS_BUCKET_NAME,
+  AWS_BUCKET_REGION,
+  AWS_ACCESS_KEY,
+  AWS_SECRET_KEY,
+} = process.env;
+
+// Database credentials enviroviment variables
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PORT,
+  DB_PASSWORD,
+  DB_NAME,
+} = process.env;
+
+// Auth configuration enviroviment variables
+const {
+  JWT_SECRET,
+} = process.env;
+
+export {
+  AWS_BUCKET_NAME,
+  AWS_BUCKET_REGION,
+  AWS_ACCESS_KEY,
+  AWS_SECRET_KEY,
+};
+
+export {
+  DB_HOST,
+  DB_USER,
+  DB_PORT,
+  DB_PASSWORD,
+  DB_NAME,
+};
+
+export {
+  JWT_SECRET,
+};
