0f6ae6a54c09ac64b2a6a3ca8d1b5930f98f1fe9
Samuel Botelho <30642802+samuelfbtlh@users.noreply.github.com>
2021-09-05 14:50:03 -0300

    Update ApiError.js

--- src/errors/ApiError.js
+++ src/errors/ApiError.js
@@ -5,4 +5,6 @@ class ApiError {
   }
 }
 // Is this class doing anything besides receiving data? what is the return?
+// Take a look at this Method in the Erro object: Error.captureStackTrace(this);
+// Also, read this: https://stackoverflow.com/questions/59625425/understanding-error-capturestacktrace-and-stack-trace-persistance
 export default ApiError;
