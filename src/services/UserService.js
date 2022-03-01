e4cda50f5681ae8737cc76a45cdfe8b0ce1a42d4
Gabriel Nascimento <gr153tt@gmail.com>
2022-03-01 18:29:15 -0300

    return user followers on get profile user service method

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,12 +1,19 @@
-import FollowersRepository from '../repositories/FollowersRepository';
 import UserRepository from '../repositories/UserRepository';
+import FriendshipsService from './FriendshipsService';
 
 const UserService = {
   getProfile: async (loggedUserID) => {
     const user = await UserRepository.findById(loggedUserID);
+    const userFollowers = await FriendshipsService.followers({ userId: user.id });
 
     delete user.password;
-    return user;
+
+    const response = {
+      user,
+      followers: userFollowers,
+    };
+
+    return response;
   },
   updateProfile: async () => {
 
