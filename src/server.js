e092cffd2ac45c3e6ab077c68f34eeff07226bbe
Gabriel Nascimento <gr153tt@gmail.com>
2022-03-01 18:21:28 -0300

    create variable to avoid magic numbers

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import http from 'http';
 import app from './app';
 
+const PORT = process.env.PORT || 3333;
+
 const server = http.createServer(app);
-server.listen(process.env.PORT || 3333);
+server.listen(PORT);
